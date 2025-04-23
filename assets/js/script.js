ca43695f08893b031b99e70248e1fbe705749b01
Ignacio <148559176+iarayae@users.noreply.github.com>
2025-04-23 01:32:00 +0000

    Se incorpora campo de nombre y promedio final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
 // Lista de Ramos //
 const ramos = ["HTML", "CSS", "JavaScript"];
 
 // Contenedor de tabla notas //
 const tabla = document.getElementById("tabla-contenido");
+
+let promedios = [];
 
 // Recorrer cada ramo //
 ramos.forEach(ramo => {
@@ -12,6 +16,7 @@ ramos.forEach(ramo => {
     const nota3 = parseFloat(prompt(`Ingresa la tercera nota de ${ramo}:`));
     // Calcular promedio //
     const promedio = ((nota1 + nota2 + nota3)/3).toFixed(1);
+    promedios.push(parseFloat(promedio));
 
     // Creación de Fila en el HTML //
     const fila = `
@@ -26,4 +31,11 @@ ramos.forEach(ramo => {
 
     // Insertando fila en tabla //
     tabla.innerHTML += fila;
+});
+
+// Promedio final (extra) //
+   const sumaFinal = promedios.reduce((acc, val) => acc + val, 0);
+   const promedioFinal = (sumaFinal / promedios.length).toFixed(1);
+   
+   document.getElementById("promedio-final").innerText = `Tu promedio final es: ${promedioFinal}`;
 });
